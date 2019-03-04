698e0810317d476071af0f1669736dc72dc45d5c
Roopak <roopak@ajirasoft.com>
2019-03-04 21:11:34 +0530

    Adds sample shipment data

---
+++ src/app/_data/shipment.ts
@@ -0,0 +1,52 @@
+export const SHIPMENT = {
+  name: 'Shipment',
+  children: [
+    {
+      name: 'a123',
+      children: [
+        {
+          name: 250,
+          value: 25000
+        },
+        {
+          name: 280,
+          children: [
+            {
+              name: 275,
+              value: 22000
+            },
+            {
+              name: 280,
+              value: 16000
+            }
+          ]
+        },
+        {
+          name: 295,
+          value: 15000
+        },
+        {
+          name: 290,
+          value: 44000
+        },
+        {
+          name: 560,
+          children: [
+            {
+              name: 275,
+              value: 22000
+            },
+            {
+              name: 275,
+              value: 18000
+            },
+            {
+              name: 285,
+              value: 20000
+            }
+          ]
+        }
+      ]
+    }
+  ]
+};
